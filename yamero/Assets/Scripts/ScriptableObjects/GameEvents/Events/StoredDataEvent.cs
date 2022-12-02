0919b11d5305b27e8e1883d52953d09fa973535e
Anthony Pereira <60923555+Anth0nyPereira@users.noreply.github.com>
2022-12-02 16:16:40 +0000

    just removed some files from SO folder that wont be used

--- yamero/Assets/Scripts/ScriptableObjects/GameEvents/Events/StoredDataEvent.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Stored Data Event", menuName = "Game Events/Stored Data Event")]
-public class StoredDataEvent : BaseGameEvent<StoredData>
-{
-}
