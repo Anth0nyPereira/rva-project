cfb5dea9fa323df324f132eb50bb548b00615c9d
Anthony Pereira <60923555+Anth0nyPereira@users.noreply.github.com>
2022-12-27 15:12:53 +0000

    now when I discard the gun, it returns to the initial Position

---
+++ yamero/Assets/Scripts/Gun.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : Collidable
+{
+    private Vector3 initPos;
+
+    public override void Awake()
+    {
+        base.Awake();
+        initPos = transform.position;
+    }
+
+    public void Update()
+    {
+        if (this.transform.position.y <= 10)
+        {
+            restartGunPosition();
+        }
+    }
+
+    public void disableCollider()
+    {
+        this.col.enabled = false;
+    }
+
+    public void enableCollider()
+    {
+        this.col.enabled = true;
+    }
+
+    private void resetDefaultPosition()
+    {
+        transform.position = initPos;
+    }
+
+    private void restartGunPosition()
+    {
+        this.enableCollider();
+        this.resetDefaultPosition();
+    }
+
+
+}
