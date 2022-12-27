49cdfab787f2ac44659bde33ab58cccd671e4c2d
Anthony Pereira <60923555+Anth0nyPereira@users.noreply.github.com>
2022-12-27 16:39:58 +0000

    doing falling down coroutine

--- yamero/Assets/Scripts/HangedMan.cs
+++ yamero/Assets/Scripts/HangedMan.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,11 +12,16 @@ public class HangedMan : Collidable
 
     private float bulletDamage;
 
+    private bool fallingDown;
+
+    private GameObject knot1;
+
     public override void Awake()
     {
         base.Awake();
         health = maxHealth.Value;
         bulletDamage = 0;
+        fallingDown = false;
     }
 
     public override void OnCollisionEnter(Collision other)
@@ -57,7 +63,52 @@ public class HangedMan : Collidable
     private void breakHangedManRope()
     {
         disableCollider();
-        GameObject knot1 = this.getKnot();
+        knot1 = this.getKnot();
         Debug.Log("Make hanged man fall down");
+        makeFallDown();
     }
+
+    public void makeFallDown()
+    {
+        fallingDown = true;
+        doFallingDownBehaviour();
+    }
+
+    public void doFallingDownBehaviour()
+    {
+        StartCoroutine(makeFallingAnimation(whenCoroutineEnds));
+
+        Debug.Log("Have a break, have a kit kat");
+    }
+
+    public float getDistanceBetweenPositions(Vector3 pos1, Vector3 pos2)
+    {
+        Vector3 directionVector = pos2 - pos1;
+        float distance = Mathf.Sqrt(Mathf.Pow(directionVector.x, 2) + Mathf.Pow(directionVector.y, 2) + Mathf.Pow(directionVector.z, 2));
+        return distance;
+    }
+
+    public IEnumerator makeFallingAnimation(Action whenCEnds)
+    {
+        Vector3 initialPosition = knot1.transform.position;
+        Debug.Log(getDistanceBetweenPositions(initialPosition, knot1.transform.position));
+        while (getDistanceBetweenPositions(initialPosition, knot1.transform.position) <= 10)
+        {
+            knot1.transform.position -= new Vector3(0, 1, 0);
+            yield return new WaitForSeconds(Time.fixedDeltaTime);
+        }
+        whenCEnds();
+    }
+
+    public void whenCoroutineEnds()
+    {
+        Debug.Log("finished coroutine");
+        stopFallingDown();
+    }
+
+    public void stopFallingDown()
+    {
+        fallingDown = false;
+    }
+
 }
