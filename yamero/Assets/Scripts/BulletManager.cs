517eb6f90894ca0126dfd263231feeebb756c772
Anthony Pereira <60923555+Anth0nyPereira@users.noreply.github.com>
2022-12-27 14:22:32 +0000

    started making the bullet spawning

---
+++ yamero/Assets/Scripts/BulletManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class BulletManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject bullet;
+
+    [SerializeField]
+    private Transform spawningPoint;
+
+    [SerializeField]
+    private float speed = 20;
+
+    private void Awake()
+    {
+        XRGrabInteractable grab = GetComponent<XRGrabInteractable>();
+        grab.activated.AddListener(FireBullet);
+    }
+
+    private void FireBullet(ActivateEventArgs args)
+    {
+        GameObject spawned = Instantiate(bullet);
+        spawned.transform.position = spawningPoint.transform.position;
+        spawned.GetComponent<Rigidbody>().velocity = spawningPoint.forward * speed;
+    }
+}
