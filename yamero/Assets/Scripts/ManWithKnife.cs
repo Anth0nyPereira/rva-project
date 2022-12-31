8dc4c8656f7b106c50c79619859121c269de95af
Anthony Pereira <60923555+Anth0nyPereira@users.noreply.github.com>
2022-12-31 00:08:09 +0000

    bug: animation of moving hands is playing on loop and the wrong object is falling down

--- yamero/Assets/Scripts/ManWithKnife.cs
+++ yamero/Assets/Scripts/ManWithKnife.cs
@@ -10,13 +10,37 @@ public class ManWithKnife : Collidable
 
     private float lighterDamage;
 
+    private Animator animator;
+
     public override void Awake()
     {
         base.Awake();
         health = maxHealth.Value;
+        animator = GetComponent<Animator>();
     }
 
     public override void OnCollisionEnter(Collision other)
+    {
+        dealWithCollision(other);
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        dealWithCollision(other);
+    }
+
+    public void Update()
+    {
+        if (health <= 0)
+        {
+            Debug.Log("Hands should move");
+            Debug.Log("Knife should fall dowm");
+            Debug.Log("Puzzle completed");
+            dealWithNoHealth();
+        }
+    }
+
+    public void dealWithCollision(Collision other)
     {
         if (other.gameObject.tag == "Lighter")
         {
@@ -27,18 +51,26 @@ public class ManWithKnife : Collidable
         }
     }
 
-    public void Update()
-    {
-        if (health == 0)
-        {
-            Debug.Log("Hands should move");
-            Debug.Log("Knife should fall dowm");
-            Debug.Log("Puzzle completed");
-        }
-    }
-
     private void TakeDamage()
     {
         health -= lighterDamage;
     }
+
+    private void dealWithNoHealth()
+    {
+        moveHands();
+        letKnifeFallDown();
+    }
+
+    private void moveHands()
+    {
+        animator.Play("moveHandsAnimation");
+    }
+
+    private void letKnifeFallDown()
+    {
+        this.transform.GetChild(0).gameObject.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+
 }
